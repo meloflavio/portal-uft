291513cb659e27002cf334af83b57c4d1c826c2a
Flávio Melo <meloflavio@gmail.com>
2022-06-23 15:13:43 -0300

    Add invariant validation

--- backend/src/portal_uft/src/portal_uft/validators.py
+++ backend/src/portal_uft/src/portal_uft/validators.py
@@ -1,5 +1,14 @@
 """Validators"""
+from portal_uft import _
+from zope.interface import Invalid
+
 import re
+
+
+class BadValue(Invalid):
+    """Exception rised when a provider value is informed"""
+
+    __doc__ = _("The value is not correct")
 
 
 def isValidEmail(value: str) -> bool:
