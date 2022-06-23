e849fe157a0e66922cf84d0681f6b10fe121af6b
Flávio Melo <meloflavio@gmail.com>
2022-06-23 17:51:38 -0300

    Add test behavior contact info

---
+++ backend/src/portal_uft/src/portal_uft/tests/test_behavior_contact_info.py
@@ -0,0 +1,31 @@
+from kitconcept import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from portal_uft.behaviors import contact_info
+from portal_uft.testing import PORTAL_UFT_INTEGRATION_TESTING
+
+import unittest
+
+
+class ContactInfoBehaviorTest(unittest.TestCase):
+
+    layer = PORTAL_UFT_INTEGRATION_TESTING
+
+    BEHAVIOR = "portal-uft.contact_info"
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer["portal"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+    def test_behavior_footer(self):
+        behavior = api.fti.get_behavior_registration(self.BEHAVIOR)
+        self.assertEqual(
+            behavior.marker,
+            contact_info.IContactInfo,
+        )
+
+    def test_applied_in_person(self):
+        portal_type = "person"
+        behaviors = api.fti.behaviors_for_type(portal_type)
+        self.assertIn(self.BEHAVIOR, behaviors)
